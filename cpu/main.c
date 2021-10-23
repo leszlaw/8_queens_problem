b87fef556ac1d01195970f0c2d4ca74cf601def7
leszlaw <leonion121974@gmail.com>
2021-10-23 13:44:20 +0200

    Create cpu project base

--- cpu/main.c
+++ cpu/main.c
@@ -1,6 +1,34 @@
 ﻿#include <stdio.h>
+#include <windows.h>
+#include <stdbool.h>
+
+#define BOARD_SIZE 8
+#define NUM_OF_THREADS 8
+
+int* create_board();
+int get_cores_num();
+bool check_position();
 
 int main()
-{
-    printf("Hello World!");
+{			
+    printf("%d", get_cores_num());
+}
+
+int* create_board() {
+	int *board = malloc(sizeof (int) * BOARD_SIZE);
+	for(int i=0; i<BOARD_SIZE; i++){
+		board[i] = i;
+	}
+	return board;
+}
+
+int get_cores_num() {
+    SYSTEM_INFO sysinfo;
+    GetSystemInfo(&sysinfo);
+    return sysinfo.dwNumberOfProcessors;
+}
+
+bool check_position() {
+	/* Implementacja funkcji sprawdzającej czy na szachownicy nic sie nie atakuje */
+	return true;
 }
